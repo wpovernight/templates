397176ec037b18e73f54d724636d1288d2e083cf
Alexandre Faustino <alexmigf@gmail.com>
2024-11-21 18:03:34 +0000

    v1.0.1

--- wpo-ips-default-templates.php
+++ wpo-ips-default-templates.php
@@ -4,14 +4,14 @@
  * Requires Plugins: woocommerce-pdf-invoices-packing-slips
  * Plugin URI:       https://wpovernight.com/downloads/woocommerce-pdf-invoices-packing-slips-bundle/
  * Description:      Default Templates for PDF Invoices & Packing Slips for WooCommerce
- * Version:          1.0.0
+ * Version:          1.0.1
  * Author:           WP Overnight
  * Author URI:       http://www.wpovernight.com
  * License:          GPLv3
  * License URI:      License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-const WPO_IPS_DEFAULT_TEMPLATES_VERSION = '1.0.0';
+const WPO_IPS_DEFAULT_TEMPLATES_VERSION = '1.0.1';
 
 function wpo_ips_default_templates_extension_path( $template_paths ) {
 	$template_paths['extension::v' . WPO_IPS_DEFAULT_TEMPLATES_VERSION] = untrailingslashit( plugin_dir_path( __FILE__ ) ) . '/';
