a5e3e0241cf5315f0466b4d0b95fe2cbac48101b
Alexandre Faustino <alexmigf@gmail.com>
2024-11-21 14:41:38 +0000

    Plugin files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Default Templates for PDF Invoices &amp; Packing Slips for WooCommerce
+
+### Manual Installation via the WordPress Admin Interface
+
+1. Download the plugin's zip file from the [**Releases**](https://github.com/wpovernight/wpo-ips-default-templates/releases) section to your computer.
+2. In your WordPress admin dashboard, navigate to **Plugins > Add New**.
+3. Click on **Upload Plugin**.
+4. Select and upload the downloaded zip file. WordPress will install the plugin automatically.
+5. Once the installation is complete, click the **Activate Plugin** link to enable it.
---
+++ wpo-ips-default-templates.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:      PDF Invoices & Packing Slips for WooCommerce - Default Templates
+ * Requires Plugins: woocommerce-pdf-invoices-packing-slips
+ * Plugin URI:       https://wpovernight.com/downloads/woocommerce-pdf-invoices-packing-slips-bundle/
+ * Description:      Default Templates for PDF Invoices & Packing Slips for WooCommerce
+ * Version:          1.0.0
+ * Author:           WP Overnight
+ * Author URI:       http://www.wpovernight.com
+ * License:          GPLv3
+ * License URI:      License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+function wpo_ips_default_templates_extension_path( $template_paths ) {
+	$template_paths[] = untrailingslashit( plugin_dir_path( __FILE__ ) ) . '/';
+	return $template_paths;
+}
+add_filter( 'wpo_wcpdf_template_paths', 'wpo_ips_default_templates_extension_path', 10, 1 );
