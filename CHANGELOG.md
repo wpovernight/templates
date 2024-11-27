b5442b2faa1a629ff5c308d3456d3dfa6f5a93b3
Alexandre Faustino <alexmigf@gmail.com>
2024-11-27 18:26:34 +0000

    New: adds Github updater class

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Changelog
+
+### v1.0.2
+
+- Adds Github updater class
+
+### v1.0.1
+
+- Adds template group and version
+
+### v1.0.0
+
+- First version
--- wpo-ips-default-templates.php
+++ wpo-ips-default-templates.php
@@ -2,17 +2,26 @@
 /**
  * Plugin Name:      PDF Invoices & Packing Slips for WooCommerce - Default Templates
  * Requires Plugins: woocommerce-pdf-invoices-packing-slips
- * Plugin URI:       https://wpovernight.com/downloads/woocommerce-pdf-invoices-packing-slips-bundle/
+ * Plugin URI:       https://github.com/wpovernight/templates
  * Description:      Default Templates for PDF Invoices & Packing Slips for WooCommerce
- * Version:          1.0.1
+ * Version:          1.0.2
+ * Update URI:       https://github.com/wpovernight/templates
  * Author:           WP Overnight
  * Author URI:       http://www.wpovernight.com
  * License:          GPLv3
  * License URI:      License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-const WPO_IPS_DEFAULT_TEMPLATES_VERSION = '1.0.1';
+const WPO_IPS_DEFAULT_TEMPLATES_VERSION = '1.0.2';
 
+// Load the updater
+require 'GitHubUpdater.php';
+$gitHubUpdater = new \WPO\GitHubUpdater\GitHubUpdater( 'templates/wpo-ips-default-templates.php' );
+$gitHubUpdater->setChangelog( 'CHANGELOG.md' );
+$gitHubUpdater->enableDebugger();
+$gitHubUpdater->add();
+
+// Add the template path
 function wpo_ips_default_templates_extension_path( $template_paths ) {
 	$template_paths['extension::v' . WPO_IPS_DEFAULT_TEMPLATES_VERSION] = untrailingslashit( plugin_dir_path( __FILE__ ) ) . '/';
 	return $template_paths;
