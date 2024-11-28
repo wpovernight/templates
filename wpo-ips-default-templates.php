f7aa2ce9869feb274ba4e579ba034a00c7e623dd
Alexandre Faustino <alexmigf@gmail.com>
2024-11-28 08:57:08 +0000

    Update wpo-ips-default-templates.php

--- wpo-ips-default-templates.php
+++ wpo-ips-default-templates.php
@@ -12,16 +12,24 @@
  * License URI:      License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
 const WPO_IPS_DEFAULT_TEMPLATES_VERSION = '1.0.2';
 
 $plugin_path           = plugin_dir_path( __FILE__ );
 $plugin_directory_name = basename( $plugin_path );
+$plugin_file		   = $plugin_directory_name . '/wpo-ips-default-templates.php';
 $github_updater_file   = $plugin_path . 'GitHubUpdater.php';
 
-// Load the updater
-if ( file_exists( $github_updater_file ) ) {
+if ( ! class_exists( '\\WPO\\GitHubUpdater\\GitHubUpdater' ) && file_exists( $github_updater_file ) ) {
 	require_once $github_updater_file;
-	$gitHubUpdater = new \WPO\GitHubUpdater\GitHubUpdater( $plugin_directory_name . '/wpo-ips-default-templates.php' );
+}
+
+// Load the updater
+if ( class_exists( '\\WPO\\GitHubUpdater\\GitHubUpdater' ) ) {
+	$gitHubUpdater = new \WPO\GitHubUpdater\GitHubUpdater( $plugin_file );
 	$gitHubUpdater->setChangelog( 'CHANGELOG.md' );
 	$gitHubUpdater->add();
 }
