47ab34a7407879a426ff7e2bf8a5c8f0d47741dc
Alexandre Faustino <alexmigf@gmail.com>
2024-11-29 08:03:26 +0000

    Removes Github updater after internal discussions.
    Includes main plugin class now.

--- wpo-ips-default-templates.php
+++ wpo-ips-default-templates.php
@@ -5,38 +5,69 @@
  * Plugin URI:       https://github.com/wpovernight/templates
  * Description:      Default Templates for PDF Invoices & Packing Slips for WooCommerce
  * Version:          1.0.2
- * Update URI:       https://github.com/wpovernight/templates
  * Author:           WP Overnight
  * Author URI:       http://www.wpovernight.com
  * License:          GPLv3
- * License URI:      License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * License URI:      http://www.gnu.org/licenses/gpl-3.0.html
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit;
 }
 
-const WPO_IPS_DEFAULT_TEMPLATES_VERSION = '1.0.2';
+if ( ! class_exists( 'WPO_IPS_Default_Templates' ) ) :
+	
+	class WPO_IPS_Default_Templates {
+		
+		/**
+		 * Version
+		 *
+		 * @var string
+		 */
+		public $version = '1.0.2';
+		
+		/**
+		 * Main instance
+		 */
+		public static function instance() {
+			if ( is_null( self::$_instance ) ) {
+				self::$_instance = new self();
+			}
+			return self::$_instance;
+		}
+		
+		/**
+		 * Constructor
+		 */
+		public function __construct() {
+			add_filter( 'wpo_wcpdf_template_paths', array( $this, 'add_template_path' ), 10, 1 );
+		}
 
-$plugin_path           = plugin_dir_path( __FILE__ );
-$plugin_directory_name = basename( $plugin_path );
-$plugin_file		   = $plugin_directory_name . '/wpo-ips-default-templates.php';
-$github_updater_file   = $plugin_path . 'GitHubUpdater.php';
+		/**
+		 * Add the template path
+		 * 
+		 * @param array $template_paths
+		 * @return array
+		 */
+		public function add_template_path( array $template_paths ): array {
+			$template_paths['extension::v' . $this->version] = untrailingslashit( plugin_dir_path( __FILE__ ) ) . '/';
+			return $template_paths;
+		}
+	}
+	
 
-if ( ! class_exists( '\\WPO\\GitHubUpdater\\GitHubUpdater' ) && file_exists( $github_updater_file ) ) {
-	require_once $github_updater_file;
+endif;
+
+/**
+ * Returns the main instance of WPO_IPS_Default_Templates
+ *
+ * @return WPO_IPS_Default_Templates
+ */
+function WPO_IPS_Default_Templates() {
+	return WPO_IPS_Default_Templates::instance();
 }
 
-// Load the updater
-if ( class_exists( '\\WPO\\GitHubUpdater\\GitHubUpdater' ) ) {
-	$gitHubUpdater = new \WPO\GitHubUpdater\GitHubUpdater( $plugin_file );
-	$gitHubUpdater->setChangelog( 'CHANGELOG.md' );
-	$gitHubUpdater->add();
-}
+WPO_IPS_Default_Templates();
 
-// Add the template path
-function wpo_ips_default_templates_extension_path( $template_paths ) {
-	$template_paths['extension::v' . WPO_IPS_DEFAULT_TEMPLATES_VERSION] = untrailingslashit( plugin_dir_path( __FILE__ ) ) . '/';
-	return $template_paths;
-}
-add_filter( 'wpo_wcpdf_template_paths', 'wpo_ips_default_templates_extension_path', 10, 1 );
+
+
